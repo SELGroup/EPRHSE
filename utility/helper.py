caae9257d7eac5b23b5f355dd2d843fa23be45f0
zhangjingyun7 <117434477+zhangjingyun7@users.noreply.github.com>
2024-11-13 15:56:14 +0800

    Update helper.py

--- utility/helper.py
+++ utility/helper.py
@@ -34,15 +34,15 @@ def convert_list_str(list):
 
 def early_stopping(log_value, best_value, stopping_step, expected_order='acc', flag_step=100):
     # early stopping strategy:
-    assert expected_order in ['acc', 'dec']  #acc:log_value正常呈增大势态，dec:log_value正常呈减小势态
+    assert expected_order in ['acc', 'dec']  
 
     if (expected_order == 'acc' and log_value >= best_value) or (expected_order == 'dec' and log_value <= best_value):
         stopping_step = 0
         best_value = log_value
     else:
-        stopping_step += 1 #出现异常次数+1
+        stopping_step += 1 
 
-    if stopping_step >= flag_step: #连续出现异常次数到达flag_step
+    if stopping_step >= flag_step: 
         print("Early stopping is trigger at step: {} log:{}".format(flag_step, log_value))
         should_stop = True
     else:
